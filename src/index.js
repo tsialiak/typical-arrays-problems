7ff7fe9a4e4fbf6ad40c9472ddac260fb305f11c
Valery Tsialiak <tsialiak.valery@gmail.com>
2021-03-09 09:59:46 +0300

    refactor: change min(), max(), avg()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length) ? array.reduce( (acc, elem) => (acc > elem) ? elem : acc) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array && array.length) ? array.reduce( (acc, elem) => (acc < elem) ? elem : acc) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array && array.length) ? array.reduce( (acc, elem) => Math.round((acc + elem / array.length) * 100) / 100, 0) : 0;
 }
